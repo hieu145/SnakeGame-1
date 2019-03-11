8ca70e08fe2f5fd2a6f8171a2badad3995bec626
sumduong <sumduong@10.250.77.64>
2019-03-11 16:44:06 -0700

    test

--- SnakeGame-1/src/snakeGame/Background.java
+++ SnakeGame-1/src/snakeGame/Background.java
@@ -4,7 +4,7 @@ import java.awt.Color;
 public class Background {
 /**
  * initializes the number of rows and columns
- * of the background asdasdsaasdsadasdas
+ * of the background 
  */
 
 final int ROW_NUM ;
