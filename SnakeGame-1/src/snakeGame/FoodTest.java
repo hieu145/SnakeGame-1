a9d309a2ab4f1105146502cb33225acc94cde2ed
HieuHoang <hieu.hoang@sjsu.edu>
2019-03-12 18:59:28 -0700

    try

---
+++ SnakeGame-1/src/snakeGame/FoodTest.java
@@ -0,0 +1,14 @@
+package MySnakeGame;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+/asdf
+class FoodTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ SnakeGame-1/src/snakeGame/SnakeTest.java
@@ -0,0 +1,14 @@
+package MySnakeGame;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+/asdf
+class SnakeTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
