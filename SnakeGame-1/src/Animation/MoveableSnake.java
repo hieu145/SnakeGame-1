59efe1fdd75f08d0963c3bfef3849bcc08843990
hieu145 <hnhan17@sjnd.org>
2019-05-06 20:48:40 -0700

    Animation changes

--- SnakeGame-1/src/Animation/MoveableSnake.java
+++ SnakeGame-1/src/Animation/MoveableSnake.java
@@ -18,22 +18,40 @@ public void draw(Graphics2D g2) {
 	Ellipse2D.Double head = new Ellipse2D.Double(x+40,10,20,20);
 	Ellipse2D.Double body = new Ellipse2D.Double(x+20, 10,20,20);
 	Ellipse2D.Double body2 = new Ellipse2D.Double(x,10,20,20);
+	Ellipse2D.Double head1 = new Ellipse2D.Double(x+40,400,20,20);
+	Ellipse2D.Double bod3 = new Ellipse2D.Double(x+20,400,20,20);
+	Ellipse2D.Double bod4 = new Ellipse2D.Double(x,400,20,20);
+
 	g2.draw(head);
+	
+	g2.draw(head1);
 	g2.draw(body);
 	g2.draw(body2);
+	g2.draw(bod3);
+	g2.draw(bod4);
 	g2.setColor(Color.BLACK);
 	g2.fill(head);
+	
+	g2.setColor(Color.BLUE);
+	g2.fill(head1);
+	g2.setColor(Color.CYAN);
+	g2.fill(body);
+	g2.fill(body2);
+	g2.setColor(Color.MAGENTA);
+	g2.fill(bod3);
+	g2.fill(bod4);
 	g2.setColor(Color.WHITE);
 	g2.setFont(new Font("TimesRoman", Font.PLAIN, 50)); 
-	g2.drawString("Welcome", 850, 450);
+	g2.drawString("Welcome", 845, 205);
 }
 public void move() {
 	x = x+speed;
+	
 	if(x>1900)
 	{
 		x=0;
 	}
-	y++;
+
 }
 	
 
--- SnakeGame-1/src/Animation/Test.java
+++ SnakeGame-1/src/Animation/Test.java
@@ -2,6 +2,7 @@ package Animation;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 //testing commit
 public class Test {
 	private static final int ICON_WIDTH = 1900;
@@ -10,17 +11,22 @@ public class Test {
 public static void main(String[] args)
 {
 	
+	String fileName = "resourse/snake2.jpg";
+	ImageIcon snakeImage = new ImageIcon(fileName);
+	JLabel snake = new JLabel(snakeImage);
 	JFrame frame = new JFrame();
+	frame.getContentPane().add(new JScrollPane(snake));
+	
 	final MoveableShape shape = new MoveableSnake(0,0,SNAKE_WIDTH);
 	ShapeIcon icon = new ShapeIcon(shape, ICON_WIDTH, ICON_HEIGHT);
 	final JLabel label = new JLabel(icon);
 	frame.setLayout(new FlowLayout());
 	frame.add(label);
+
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.pack();
 	frame.setVisible(true);
 	frame.getContentPane().setBackground(Color.PINK);
-	
+
 	final int delay = 10;
 	Timer t = new Timer(delay, event -> 
 	{
@@ -29,6 +35,7 @@ public static void main(String[] args)
 		});
 			t.start();
 }
+
 }
 	
 
