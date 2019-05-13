f805c08a82981330719b80ac3bb5b0cd40c8756d
sumduong <sumduong@10.250.19.174>
2019-05-13 15:07:02 -0700

    Update Code

---
+++ SnakeGame-1/src/edu/sjsu/cs/cs151/MainGame.java
@@ -0,0 +1,36 @@
+package edu.sjsu.cs.cs151;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import javax.*;
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+import edu.sjsu.cs.cs151.controller.Controller;
+import edu.sjsu.cs.cs151.model.Model;
+import edu.sjsu.cs.cs151.view.MainView;
+
+public class MainGame {
+	private static MainView view;
+	private static Model model;
+	private static BlockingQueue<Message>queue = new LinkedBlockingQueue<Message>();
+	public static void main(String[] args) throws Exception {
+		view = new MainView(queue);
+		model = new Model();
+		Controller controller = new Controller(view, model, queue);
+		try {
+		controller.mainLoop();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		view.clear();
+		queue.clear();	
+	}
+}
+
